275a4eb880a9e2551e11691d6bb4eca6d2403ac8
mirapach <pacheco.mirellaf@gmail.com>
2026-06-10 19:45:50 -0300

    Create ex08.py

---
+++ ex08.py
@@ -0,0 +1,4 @@
+peso = float(input("Qual é o seu peso em kg? "))
+altura = float(input("Qual é a sua altura em metros? "))
+imc = peso / altura**2
+print(f"O seu Índice de Massa Corporal é de {imc} kg/m²!")
