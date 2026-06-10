694d63d4cf01e283f31b50ff43bcb745ca05f1aa
mirapach <pacheco.mirellaf@gmail.com>
2026-06-10 19:45:17 -0300

    Create ex06.py

---
+++ ex06.py
@@ -0,0 +1,4 @@
+ano_nasci = int(input("Insira o ano do seu nascimento: "))
+ano_atual = int(input("Insira o ano atual: "))
+idade = ano_atual-ano_nasci
+print(f"Você está fazendo {idade} anos! Parabéns.")
