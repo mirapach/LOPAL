618e160ecd9ce31835a47173ac218345640253ce
mirapach <pacheco.mirellaf@gmail.com>
2026-06-10 19:47:01 -0300

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,4 @@
+lista = ["kalebe.barros@gmail.com", "tiago.romano@escola.br", "kamilly.rosa@escola.br", "rafaela.godoy@gmail.com"]
+for i in lista:
+    if i.endswith("@escola.br"):
+        print(i)
