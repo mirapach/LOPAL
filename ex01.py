d7460ae6ed1c47470b73f2595dd2393a8ae28858
mirapach <pacheco.mirellaf@gmail.com>
2026-06-10 19:43:09 -0300

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,1 +1,4 @@
-
+reais = float(input("Insira um valor em reais: "))
+cotacao_dolar = float(input("Insira a cotação atual do dólar: "))
+dolares = reais*cotacao_dolar
+print(f"{reais} reais equivalem a {dolares} dólares")
